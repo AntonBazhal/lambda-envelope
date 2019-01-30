30219ed06e733a98b40e52b1c73ad310de8de513
AntonBazhal <am.bazhal@gmail.com>
2019-01-29 20:04:58 -0500

    Added ResponseBuilder

--- README.md
+++ README.md
@@ -18,15 +18,16 @@ $ npm install lambda-envelope
 
 ### Response
 
-[AWS Lambda][aws-lambda-url] does not provide a way to separate client and server errors, as errors can't be extended with custom fields. `Response` class contains methods needed by [AWS Lambda][aws-lambda-url] to correctly serialize custom response or error as well as helper factory method to parse raw [AWS Lambda][aws-lambda-url] response. Successful results and client errors should be returned using [success callback](lambda-callback-url). Errors that should be treated as server errors and, as a result, be picked up by [Amazon CloudWatch][cloudwatch-url], must be returned using [error callback](lambda-callback-url).
+[AWS Lambda][aws-lambda-url] does not provide a way to separate client and server errors, as errors can't be extended with custom fields. `Response` class contains methods needed by [AWS Lambda][aws-lambda-url] to correctly serialize custom response or error. Successful results and client errors should be returned using [success callback](lambda-callback-url). Errors that should be treated as server errors and, as a result, be picked up by [Amazon CloudWatch][cloudwatch-url], must be returned using [error callback](lambda-callback-url).
 
-### constructor(options)
+#### constructor(options)
 Constructor takes options object with optional parameters:
 
 - **[statusCode]** - response status code that should match HTTP status codes, but can be any proprietary value [defaults to **`200`**]
+- **[encoding]** - response body encoding [defaults to **`identity`**]
 - **[body]** - response body (can be of any type that is `JSON.stringify` compatible) [defaults to **`{}`**]
 
-#### Example
+##### Example
 ```js
 const Response = require('lambda-envelope').Response;
 
@@ -42,13 +43,80 @@ module.exports.handler = function(event, context, callback) {
 }
 ```
 
-### fromAWSResponse(awsResponse)
-Factory method that handles raw [AWS Lambda][aws-lambda-url] invocation response parsing.
+### ResponseBuilder
 
-#### Example
+`ResponseBuilder` is a factory class that helps to deal with large response size, since [AWS Lambda][aws-lambda-url] has a [6 MB max response size][aws-lambda-limits-url] (256 KB in the asynchronous mode). It tries to compress the response when it exceeds pre-defined threshold and uploads compressed response to S3 bucket when compression does not bring response size below threshold (returning pre-signed URL). Additionally, `ResponseBuilder` class contains helper method to parse raw [AWS Lambda][aws-lambda-url] response.
+
+#### constructor(options)
+Constructor takes options object with the following parameters:
+- **bucket** - S3 bucket name to upload large responses to. This bucket should have lifecycle policy in place to cleanup old responses as soon as possible
+- **[s3client]** - an optional instance of [AWS S3 client][aws-s3-client-url] to use
+- **[threshold]** - an optional threshold in bytes to use for compression or S3 upload [defaults to **`6291456 (6 MB)`**]
+- **[urlTTL]** - an optional [pre-signed S3 URL][aws-s3-get-signed-url] expiration time in seconds [defaults to **`30`**]
+
+##### Example
 ```js
 const AWS = require('aws-sdk');
-const Response = require('lambda-envelope').Response;
+const ResponseBuilder = require('lambda-envelope').ResponseBuilder;
+
+const builder = new ResponseBuilder({
+  bucket: 'bucket-for-responses',
+  s3client: new AWS.S3({ maxRetries: 5 }),
+  threshold: 256
+  urlTTL: 60
+});
+
+module.exports.handler = function(event, context, callback) {
+  const builder = new ResponseBuilder({
+    bucket: 'bucket-for-responses',
+    threshold: 256
+  });
+
+  const response = builder.build({
+    statusCode: 200,
+    body: {
+      data: 'some data'
+    }
+  });
+
+  callback(null, response);
+}
+```
+
+#### build(options)
+Creates and instance of `Response` with the following parameters:
+
+- **[statusCode]** - response status code that should match HTTP status codes, but can be any proprietary value [defaults to **`200`**]
+- **[encoding]** - response body encoding [defaults to **`identity`**]
+- **[body]** - response body (can be of any type that is `JSON.stringify` compatible) [defaults to **`{}`**]
+
+##### Example
+```js
+const ResponseBuilder = require('lambda-envelope').ResponseBuilder;
+
+const builder = new ResponseBuilder({
+  bucket: 'bucket-for-responses'
+});
+
+module.exports.handler = function(event, context, callback) {
+  const response = builder.build({
+    statusCode: 200,
+    body: {
+      data: 'some data'
+    }
+  });
+
+  callback(null, response);
+}
+```
+
+#### fromAWSResponse(awsResponse)
+Method that handles raw [AWS Lambda][aws-lambda-url] invocation response parsing, including compressed and S3 responses.
+
+##### Example
+```js
+const AWS = require('aws-sdk');
+const ResponseBuilder = require('lambda-envelope').ResponseBuilder;
 
 const lambda = new AWS.Lambda();
 const params = {
@@ -58,7 +126,7 @@ const params = {
 
 return lambda.invoke(params)
   .promise()
-  .then(rawResponse => Response.fromAWSResponse(rawResponse))
+  .then(rawResponse => ResponseBuilder.fromAWSResponse(rawResponse))
   .then(response => {
     if (response.statusCode === 200) {
       /*success*/
@@ -72,7 +140,7 @@ return lambda.invoke(params)
 
 The MIT License (MIT)
 
-Copyright (c) 2017 Anton Bazhal
+Copyright (c) 2017-2019 Anton Bazhal
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -81,6 +149,9 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 [aws-lambda-url]: https://aws.amazon.com/lambda/details/
+[aws-lambda-limits-url]: https://docs.aws.amazon.com/lambda/latest/dg/limits.html
+[aws-s3-client-url]: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+[aws-s3-get-signed-url]: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getSignedUrl-property
 [ci-image]: https://circleci.com/gh/AntonBazhal/lambda-envelope.svg?style=shield&circle-token=f6c189b6f4e3d0e664a7947ec3e7c7e5086af079
 [ci-url]: https://circleci.com/gh/AntonBazhal/lambda-envelope
 [cloudwatch-url]: https://aws.amazon.com/cloudwatch/
--- test/Response-tests.js
+++ test/Response-tests.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 
-const Response = require('../').Response;
+const { Response } = require('../');
 
 describe('Response', function() {
   describe('#constructor', function() {
@@ -23,220 +23,43 @@ describe('Response', function() {
 
   describe('#toJSON', function() {
     it('should return proper JSON object', function() {
-      const testBody = { data: 'some data' };
-      const testEncoding = 'gzip';
-      const testStatusCode = 200;
+      const body = { data: 'some data' };
+      const encoding = 'gzip';
+      const statusCode = 200;
 
       const response = new Response({
-        statusCode: testStatusCode,
-        encoding: testEncoding,
-        body: testBody
+        statusCode,
+        encoding,
+        body
       });
 
       expect(response.toJSON()).to.be.deep.equal({
-        statusCode: testStatusCode,
-        encoding: testEncoding,
-        body: testBody
+        statusCode,
+        encoding,
+        body
       });
     });
   });
 
   describe('#toString', function() {
     it('should return proper stringified representation', function() {
-      const testBody = { data: 'some data' };
-      const testEncoding = 'gzip';
-      const testStatusCode = 200;
+      const body = { data: 'some data' };
+      const encoding = 'gzip';
+      const statusCode = 200;
 
       const response = new Response({
-        statusCode: testStatusCode,
-        encoding: testEncoding,
-        body: testBody
+        statusCode,
+        encoding,
+        body
       });
 
       const expectedResult = JSON.stringify({
-        statusCode: testStatusCode,
-        encoding: testEncoding,
-        body: testBody
+        statusCode,
+        encoding,
+        body
       });
 
       expect(response.toString()).to.be.equal(expectedResult);
-    });
-  });
-
-  describe('#fromAWSResponse', function() {
-    it('should throw when payload can`t be parsed', function() {
-      const testAWSResponse = {
-        Payload: ''
-      };
-
-      expect(() => Response.fromAWSResponse(testAWSResponse))
-        .to.throw('failed to parse response payload');
-    });
-
-    it('should use payload as body if payload is not an object', function() {
-      const testPayload = 'data';
-      const testAWSResponse = {
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 200);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body', testPayload);
-    });
-
-    it('should use payload as body if unhandled error is returned', function() {
-      const testPayload = {
-        data: 'test data'
-      };
-      const testAWSResponse = {
-        FunctionError: 'Unhandled',
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 500);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body').that.deep.equals(testPayload);
-    });
-
-    it('should use payload as body if handled error is returned, but payload has more than one field', function() {
-      const testPayload = {
-        errorMessage: 'message',
-        data: 'test data'
-      };
-      const testAWSResponse = {
-        FunctionError: 'Handled',
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 500);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body').that.deep.equals(testPayload);
-    });
-
-    it('should use payload as body if handled error is returned, but there is no errorMessage field in payload', function() {
-      const testPayload = {
-        data: 'test data'
-      };
-      const testAWSResponse = {
-        FunctionError: 'Handled',
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 500);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body').that.deep.equals(testPayload);
-    });
-
-    it('should use errorMessage as body if handled error is returned, but errorMessage can`t be parsed', function() {
-      const testPayload = {
-        errorMessage: 'abc'
-      };
-      const testAWSResponse = {
-        FunctionError: 'Handled',
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 500);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body').that.equals(testPayload.errorMessage);
-    });
-
-    it('should use parsed errorMessage as body if errorMessage can be parsed, but has no body field in it', function() {
-      const messageData = {
-        data: 'some data'
-      };
-      const testAWSResponse = {
-        FunctionError: 'Handled',
-        Payload: JSON.stringify({
-          errorMessage: JSON.stringify(messageData)
-        })
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 500);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body').that.deep.equals(messageData);
-    });
-
-    it('should use parsed body and encoding fields from errorMessage if errorMessage can be parsed', function() {
-      const messageData = {
-        body: { data: 'some data' },
-        encoding: 'gzip'
-      };
-      const testAWSResponse = {
-        FunctionError: 'Handled',
-        Payload: JSON.stringify({
-          errorMessage: JSON.stringify(messageData)
-        })
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 500);
-      expect(response).to.have.property('encoding', messageData.encoding);
-      expect(response).to.have.property('body').that.deep.equals(messageData.body);
-    });
-
-    it('should use payload as body if there is no body field in it', function() {
-      const testPayload = {
-        data: 'test data'
-      };
-      const testAWSResponse = {
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', 200);
-      expect(response).to.have.property('encoding', 'identity');
-      expect(response).to.have.property('body').that.deep.equals(testPayload);
-    });
-
-    it('should use statusCode, encoding and body from payload when present', function() {
-      const testPayload = {
-        statusCode: 201,
-        encoding: 'gzip',
-        body: {
-          data: 'test data'
-        }
-      };
-      const testAWSResponse = {
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', testPayload.statusCode);
-      expect(response).to.have.property('encoding', testPayload.encoding);
-      expect(response).to.have.property('body').that.deep.equals(testPayload.body);
-    });
-
-    it('should use statusCode, encoding and body from falsy payload when present', function() {
-      const testPayload = {
-        statusCode: 201,
-        encoding: 'gzip',
-        body: false
-      };
-      const testAWSResponse = {
-        Payload: JSON.stringify(testPayload)
-      };
-
-      const response = Response.fromAWSResponse(testAWSResponse);
-
-      expect(response).to.have.property('statusCode', testPayload.statusCode);
-      expect(response).to.have.property('encoding', testPayload.encoding);
-      expect(response).to.have.property('body').that.deep.equals(testPayload.body);
     });
   });
 });
